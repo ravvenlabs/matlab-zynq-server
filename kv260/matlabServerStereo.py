7c9a8529c089a145da1695a3e4b5d06782dc67a2
drkaputa <dskiee@rit.edu>
2026-01-02 21:37:30 -0500

    updated matlabServerStereo.py in the kv260 directory to handle python3 byte transfers.

--- kv260/matlabServerStereo.py
+++ kv260/matlabServerStereo.py
@@ -1,6 +1,7 @@
 # Dr. Kaputa
 # Matlab Server
 from numpysocket import NumpySocket
+import os
 import cv2
 import numpy as np
 import time
@@ -25,38 +26,40 @@ npSocket.startServer(9999)
 simulink = True    
 if simulink == True:
     f1 = open("/dev/mem", "r+b")
-    simulinkMem = mmap.mmap(f1.fileno(), 1000, offset=0x43c60000)
-    simulinkMem.seek(0) 
-    simulinkMem.write(struct.pack('l', 1))       # reset IP core
-    simulinkMem.seek(8)                         
-    simulinkMem.write(struct.pack('l', 752))     # image width
-    simulinkMem.seek(12)                        
-    simulinkMem.write(struct.pack('l', 480))     # image height
-    simulinkMem.seek(16)                        
-    simulinkMem.write(struct.pack('l', 94))       #  horizontal porch
-    simulinkMem.seek(20)                        
-    simulinkMem.write(struct.pack('l', 1000))     #  vertical porch when reading from debug
-    simulinkMem.seek(4) 
-    simulinkMem.write(struct.pack('l', 1))       # enable IP core
+    simulinkMem = mmap.mmap(f1.fileno(), 1000, offset=0xa0060000)
+    mv = memoryview(simulinkMem).cast('Q') 
+    mv[0] = 0x0000000100000000
+    
+    # simulinkMem.seek(0) 
+    # simulinkMem.write(struct.pack('l', 1))       # reset IP core
+    # simulinkMem.seek(8)                         
+    # simulinkMem.write(struct.pack('l', 752))     # image width
+    # simulinkMem.seek(12)                        
+    # simulinkMem.write(struct.pack('l', 480))     # image height
+    # simulinkMem.seek(16)                        
+    # simulinkMem.write(struct.pack('l', 94))       #  horizontal porch
+    # simulinkMem.seek(20)                        
+    # simulinkMem.write(struct.pack('l', 1000))     #  vertical porch when reading from debug
+    # simulinkMem.seek(4) 
+    # simulinkMem.write(struct.pack('l', 1))       # enable IP core
 
-print "entering main loop"
+print("entering main loop")
 
 # feel free to modify this command structue as you wish.  It might match the 
 # command structure that is setup in the Matlab side of things on the host PC.
 while(1):
     cmd = npSocket.receiveCmd()
-    #print(cmd)
-    if cmd == '0':
+    if cmd == b'0':
         data = npSocket.receive()
         camWriter.setFrame(data)
         npSocket.send(np.array(2))
-    elif cmd == '1':
+    elif cmd == b'1':
         frameLeft,frameRight = camFeedthrough.getStereoGray()
         tempImageLeft = np.ascontiguousarray(frameLeft, dtype=np.uint8) 
         tempImageRight = np.ascontiguousarray(frameRight, dtype=np.uint8) 
         npSocket.send(tempImageLeft)
         npSocket.send(tempImageRight)
-    elif cmd == '2':
+    elif cmd == b'2':
         frameLeft,frameRight = camProcessed.getStereoGray()
         tempImageLeft = np.ascontiguousarray(frameLeft, dtype=np.uint8) 
         tempImageRight = np.ascontiguousarray(frameRight, dtype=np.uint8) 
